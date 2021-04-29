3397034dd2fb4aaa18c01a619595d7c5f16ccd5a
efiVanso <efi.vanso@gmail.com>
2021-04-29 21:56:16 +0300

    add dashboard if the user connected

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,16 @@
 import React from "react";
 
 import Login from "./Login";
+import Dashboard from './Dashboard'
 import useLocalStorage from "../hooks/useLocalStorage";
 
 function App() {
-const [id, setId] = useLocalStorage('id')
+const [id, setId] = useLocalStorage('id');
 
-  return (
-    <>
-    {id}
-    <Login onIdSubmit={setId} />
-    </>
-  );
+  return (  <>
+      {id? <Dashboard id={id}/> :  <Login onIdSubmit={setId} />}
+
+  </>)
 }
 
 export default App;
---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Dashboard({id}) {
+    return (
+        <div>
+{id}        </div>
+    )
+}
