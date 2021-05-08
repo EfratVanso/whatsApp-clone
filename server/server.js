d56e93dfa172b4cc786bf8949253f50f7d6eb667
unknown <efi.vanso@gmail.com>
2021-05-09 02:40:34 +0300

    add server code
    with socket.io

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const io = require('socket.io')(5000)
+
+io.on('connection', socket => {
+    const id = socket.handshake.query.id //socketIo creates id 
+    socket.join(id)
+
+
+    socket.on('send-message', ({ recipients, text }) => {
+        recipient.forEach(recipient => {
+            const newRecipients = recipients.filter(r => r !== recipient)
+            newRecipients.push(id)
+            socket.broadcast.to(recipient).emit(
+                'recieve-message', { recipients: newRecipients, sender: id, text })
+        });
+    })
+})
+console.log('connected')
